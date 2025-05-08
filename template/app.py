bff308a915242d94d4cd60f20576100b5aabad0d
neotusca <neotusca@gmail.com>
2025-05-08 14:20:41 +0900

    Update app.py

--- template/app.py
+++ template/app.py
@@ -6,7 +6,8 @@
     :license: MIT, see LICENSE for more details.
 """
 import os
-from flask import Flask, render_template, flash, redirect, url_for, Markup
+from flask import Flask, render_template, flash, redirect, url_for
+from markupsafe import Markup
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'secret string')
